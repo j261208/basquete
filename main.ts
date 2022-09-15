07505616caaa39dffa72132294732c164d9df969
j261208 <113639488+j261208@users.noreply.github.com>
2022-09-15 17:29:37 -0300

    Atualizar 'main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -1,1 +1,19 @@
-
+input.onPinPressed(TouchPin.P0, function () {
+    basic.showString("prepare-se")
+    basic.pause(500)
+    basic.showNumber(3)
+    basic.showNumber(2)
+    basic.showNumber(1)
+    basic.clearScreen()
+    basic.pause(randint(1000, 7000))
+    basic.showLeds(`
+        . # # # .
+        # # # # #
+        # # # # #
+        # # # # #
+        . # # # .
+        `)
+})
+basic.forever(function () {
+	
+})
