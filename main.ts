c08885fb15075fc724320e427a2c202e112547f6
j261208 <113639488+j261208@users.noreply.github.com>
2022-09-22 17:37:49 -0300

    Atualizar 'main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
+let p2 = 0
+let p1 = 0
 input.onPinPressed(TouchPin.P0, function () {
-    basic.showString("prepare-se")
-    basic.pause(500)
+    basic.showString("PREPARE-SE")
     basic.showNumber(3)
     basic.showNumber(2)
     basic.showNumber(1)
@@ -8,11 +9,34 @@ input.onPinPressed(TouchPin.P0, function () {
     basic.pause(randint(1000, 7000))
     basic.showLeds(`
         . # # # .
-        # # # # #
+        # # # . #
         # # # # #
         # # # # #
         . # # # .
         `)
+    basic.pause(500)
+})
+input.onPinPressed(TouchPin.P2, function () {
+    basic.showLeds(`
+        . . # # #
+        . . # # #
+        . . # # #
+        . . . # #
+        . . . # #
+        `)
+    basic.clearScreen()
+    p2 += 1
+})
+input.onPinPressed(TouchPin.P1, function () {
+    basic.showLeds(`
+        # # # . .
+        # # # . .
+        # # # . .
+        # # . . .
+        # # . . .
+        `)
+    basic.clearScreen()
+    p1 += 1
 })
 basic.forever(function () {
 	
